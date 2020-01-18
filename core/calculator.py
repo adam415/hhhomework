53b4a7941c20c32a0b12133a87e7708a14de734f
Адам Мизаушев <adam415@inbox.ru>
2020-01-18 21:57:06 +0300

    Task 1: Calculator

--- core/calculator.py
+++ core/calculator.py
@@ -1,15 +1,52 @@
 from decorators import cache_decorator
 
-def calculator(a, b, operation):
-    # Здесь нужно реализовать функцию,
-    # которая реализует основные арифметические операции между числами: +, -, /, *, **.
-    # Так же следует сделать проверку, что поступивший оператор корректен (присутствует в этом списке +, -, /, *, **)
-    return a + b
+def allAreOneOf(vals, types):
+    return all(isinstance(val, types) for val in vals)
 
+def inputNum():
+    # В итоге мне показалось, что отловить исключение проще, чем по регулярке проверять
+    while True:
+        inp = input('Введите десятичное число: ')
+
+        try:
+            result = float(inp)
+            break
+        except:
+            print('Не удалось распознать введенное число :(')
+    
+    return result
+
+def calculator(a, b, operation):
+    if not allAreOneOf((a, b), (int, float, complex)):
+        return 'Invalid operands'
+    if operation not in calculator.__operations__:
+        return 'Invalid operation'
+
+    if operation in ['/', '//', '%'] and b == 0:
+        return 'Undefined result'
+    if operation == '%' and (isinstance(a, complex) or isinstance(b, complex)):
+        return 'Undefined result'
+
+    return calculator.__operations__[operation](a, b)
+
+# Типа private и каждый раз словарь не создавать
+calculator.__operations__ = {
+    '+' :   lambda x, y: x + y,
+    '-' :   lambda x, y: x - y,
+    '*' :   lambda x, y: x * y,
+    '/' :   lambda x, y: x / y,
+    '//':   lambda x, y: x // y,
+    '%' :   lambda x, y: x % y,
+    '**':   lambda x, y: x ** y
+}
 
 if __name__ == '__main__':
-    a = int(input('Введите число: ')) # Тут было бы неплохо обрабатывать ошибку в случае передачи некорректных символов
-    b = int(input('Введите число: '))
-    operation = input('Введите операцию')
 
-    print('Результат: ', calculator(a, b, operation))
+    a = inputNum()
+    b = inputNum()
+    operation = input('Введите оператор: ')
+
+    print('{} {} {} = {}'.format(
+        a, operation, b, 
+        calculator(a, b, operation)
+    ))
