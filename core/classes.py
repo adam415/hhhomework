989b40979c5dca5d80cda0c0e4dd9b7d6ad38450
Адам Мизаушев <adam415@inbox.ru>
2020-01-18 23:05:55 +0300

    Task 2: Classes

--- core/classes.py
+++ core/classes.py
@@ -1,11 +1,54 @@
 class Car:
     # Реализовать класс машины Car, у которого есть поля: марка и модель автомобиля
     # Поля должны задаваться через конструктор
-    pass
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def __repr__(self):
+        return "{{brand: '{}', model: '{}'}}".format(self.brand, self.model)
+
 
 class Garage:
     # Написать класс гаража Garage, у которого есть поле списка машин
     # Поле должно задаваться через конструктор
     # По аналогии с классом Company из лекции реализовать интерфейс итерируемого
     # Реализовать методы add и delete(удалять по индексу) машин из гаража
-    pass
+    def __init__(self, cars = []):
+        self.cars = cars
+    
+    def __len__(self):
+        return len(self.cars)
+    
+    def __getitem__(self, index):
+        return self.cars[index]
+
+    def __repr__(self):
+        result = '['
+
+        for car in self.cars:
+            result += str(car) + ', '
+
+        return result.rstrip(', ') + ']'
+
+    def add(self, car):
+        self.cars.append(car)
+
+    def delete(self, index):
+        del self.cars[index]
+
+
+g = Garage([
+    Car('B1', 'M1'),
+    Car('B2', 'M2'),
+    Car('B3', 'M3')
+])
+
+print(g)
+for c in g: print(c)
+
+g.add(Car('B4', 'C4'))
+g.add(Car('B5', 'C5'))
+g.delete(2)
+
+print(g)
